03534d79606cc561e4a15dd0490c6fd9507955d9
Diallo <thierno-sadialiou.diallo@capgemini.com>
2023-01-23 10:30:24 +0100

    put code

--- force-app/main/default/lwc/webService/webService.js
+++ force-app/main/default/lwc/webService/webService.js
@@ -1,4 +1,4 @@
 import { LightningElement } from 'lwc';
-import webService from '@salesforce/apex/CaseManager.getCaseById';
+/* import webService from '@salesforce/apex/CaseManager.getCaseById'; */
 
 export default class WebService extends LightningElement {}
